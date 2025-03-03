b6fa8820794456f37f6f650018acf3c8c3fba7dc
idris10215 <mohammedidris10215@gmail.com>
2025-03-03 07:05:29 +0530

    Updated the code

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def processCommand(c):
+    if "open google" in c.lower():
+        speak("Opening Google")
+        webbrowser.open("https://www.google.com")
+    elif "open youtube" in c.lower():
+        speak("Opening Youtube")
+        webbrowser.open("https://www.youtube.com")
+    elif "open facebook" in c.lower():
+        speak("Opening Facebook")
+        webbrowser.open("https://www.facebook.com")
+    elif "open instagram" in c.lower():
+        speak("Opening Instagram")
+        webbrowser.open("https://www.instagram.com")
+    elif "open linkedin" in c.lower():
+        speak("Opening Linkedin")
+        webbrowser.open("https://www.linkedin.com")
+
+
 
 if __name__ == "__main__":
     speak("Hello, I am Jarvis your personal assistant")
@@ -21,7 +39,16 @@ if __name__ == "__main__":
             with sr.Microphone() as source:
                 print("Listening...")
                 audio = r.listen(source, timeout=2, phrase_time_limit=2)
-            command = r.recognize_google(audio)
-            print(command)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("Yaa")
+                # listen to command
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+            
         except Exception as e:
             print("Error; {0}".format(e))        
